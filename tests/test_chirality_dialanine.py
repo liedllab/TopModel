af5d4cf111e8eaab422e121362fb4fcd6fb06b79
Janik Kokot <38459178+janikkokot@users.noreply.github.com>
2022-07-29 15:33:12 +0200

    Rename and update tests for alanine case

--- tests/test_chirality_dialanine.py
+++ tests/test_chirality_dialanine.py
@@ -16,15 +16,18 @@ with open(BASE / "config.yaml") as f:
 parser = PDBParser()
 
 
-@given(st.integers(min_value=1, max_value=8))
-def test_chirality_alanine_dipeptide(n: int):
+@given(
+        n = st.integers(min_value=1, max_value=8),
+        res_number = st.integers(min_value=0, max_value=CONFIG["n_res"]),
+        )
+def test_chirality_alanine_dipeptide(n: int, res_number: int):
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", category=PDBConstructionWarning)
         pdb = parser.get_structure(n, BASE / f"{n}.pdb")
 
-    chiralities = chirality.get_chiralities(pdb)
-    target = CONFIG[n]["stereo"]
-    assert [chirality for _, chirality in chiralities.values()] == target
+    residue = list(pdb.get_residues())[res_number]
+
+    assert chirality.assign_chirality(residue) == CONFIG[n]["chirality"][res_number]
 
 if __name__ == '__main__':
     test_alanine()
