a0a1cc6667e9435840c5273c82b2c63080e70323
Janik Kokot <38459178+janikkokot@users.noreply.github.com>
2022-07-29 15:53:44 +0200

    Add assign_chirality to pass the tests

--- pdbear/chirality.py
+++ pdbear/chirality.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 import numpy as np
-from Bio.PDB.Structure import Structure
+from Bio.PDB.Residue import Residue
 
 
 def set_zero_point(values: np.ndarray, zero_point: np.ndarray) -> np.ndarray:
@@ -19,6 +19,23 @@ def get_projector(vector: np.ndarray) -> np.ndarray:
         q = -1 * q
     return q.T
 
+def assign_chirality(residue: Residue) -> str:
+
+    atoms = {atom.name : atom.coord.copy() for atom in residue.get_atoms() \
+             if atom.name in ('C', 'CA', 'CB', 'N', 'HA')}
+    zero = atoms.pop('CA')
+    for atom, position in atoms.items():
+        atoms[atom] = set_zero_point(position, zero)
+
+    transformer = get_projector(atoms.pop('HA'))[1:]
+    
+    get_theta = lambda pos: np.mod(np.arctan2(*reversed(transformer @ pos)), 2*np.pi)
+    theta_zero = get_theta(atoms['N'])
+    rotations = {atom : get_theta(position) - theta_zero for atom, position in atoms.items()}
+
+    label = 'L' if rotations['C'] < rotations['CB'] else 'D'
+
+    return label
 
 
 def get_chiralities(pdb: Structure) -> dict[int, tuple[str, str]]:
