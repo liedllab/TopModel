21fb4e23bccce3c1ef8d473b38fed027d485b7f7
Janik Kokot <38459178+janikkokot@users.noreply.github.com>
2022-07-31 09:20:08 +0200

    Add new tests and logger for debuging
    Testcases with bigger structure still fail. Further test has been added
    that checks for the determinant of the transformation matrix. Now some
    lines of code have been added to fulfill this test. But now enough tests
    have been added to test different implementations of the get_transform
    function.
    
    Things that may be responsible for the bugs:
            - QR factorization does not consistently yield a basis with same
              orientation ( maybe its not intended to do so and dialanine
              was just luck)
            - -1 to ensure that HA is behind CA and the rest might cause
              issues, now replaced by rotation matrix multiplication.
    
    Things to try instead:
            - rotation matrices
            - quarternions

--- pdbear/chirality.py
+++ pdbear/chirality.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import numpy as np
 from Bio.PDB.Residue import Residue
-
+from .logger import logger
 
 def set_zero_point(values: np.ndarray, zero_point: np.ndarray) -> np.ndarray:
     return values - zero_point
@@ -13,11 +13,14 @@ def get_transform(vector: np.ndarray) -> np.ndarray:
              [0, 0, 1]]
             )
 
-
     q, r = np.linalg.qr(A.T)
-    if np.sign(q.T @ vector)[0] == 1:
-        q = -1 * q
-    return q.T
+    transform = q.T
+    if np.sign(transform @ vector)[0] == 1:
+        rot = np.array([[-1,0,0],[0,1,0],[0,0,1]])
+        transform = rot @ transform
+    if np.sign(np.linalg.det(transform)) == -1:
+        transform[[1,2]] = transform[[2,1]]
+    return transform
 
 
 def get_theta(position: np.ndarray) -> float:
@@ -25,22 +28,30 @@ def get_theta(position: np.ndarray) -> float:
     normalized_theta = np.mod(theta, 2*np.pi)
     return normalized_theta
 
-def assign_chirality_amino_acid(residue: Residue) -> str:
 
-    atoms = {atom.name : atom.coord.copy() for atom in residue.get_atoms() \
+def assign_chirality_amino_acid(residue: Residue) -> str:
+    atoms = {atom.name: atom.coord.copy() for atom in residue.get_atoms() \
                 if atom.name in ('C', 'CA', 'CB', 'N', 'HA')}
-    zero = atoms.pop('CA')
+    zero = atoms['CA'].copy()
     for atom, position in atoms.items():
         atoms[atom] = set_zero_point(position, zero)
 
-    transformer = get_transform(atoms.pop('HA'))[1:]
+    transformer = get_transform(atoms['HA'])
+    factor = np.sign(np.linalg.det(transformer))
+    transformed_pos = {atom: transformer @ position for atom, position in atoms.items()}
+
+    theta_zero = get_theta(transformed_pos['N'][1:])
+    rotations = {atom : get_theta(position[1:]) - theta_zero \
+                    for atom, position in transformed_pos.items()}
     
-    theta_zero = get_theta(transformer @ atoms['N'])
-    rotations = {atom : get_theta(transformer @ position) - theta_zero \
-                    for atom, position in atoms.items()}
-
-    chirality = 'L' if rotations['C'] < rotations['CB'] else 'D'
-
+    if rotations['C'] < rotations['CB']:
+        chirality = 'L'
+    else:
+        chirality = 'D'
+    
+    logger.warning(
+        f"AminoAcid {residue} has been asigned to be {chirality}\
+            {np.linalg.det(transformer)=}")
     return chirality
 
 
--- tests/test_chirality_modelled.py
+++ tests/test_chirality_modelled.py
@@ -5,7 +5,7 @@ import yaml
 from Bio.PDB.PDBParser import PDBParser
 from Bio.PDB.PDBExceptions import PDBConstructionWarning
 
-from hypothesis import given
+from hypothesis import given, assume
 from hypothesis.strategies import integers, sampled_from, composite
 
 from pdbear import chirality
@@ -21,7 +21,7 @@ parser = PDBParser()
 @composite
 def example_data(draw, examples: dict):
     key = draw(sampled_from(list(examples)))
-    res = draw(integers(min_value=1, max_value=examples[key]['res_number']))
+    res = draw(integers(min_value=examples[key]['min_res'], max_value=examples[key]['max_res']))
 
     return key, examples[key]['pdb'], res
 
@@ -34,6 +34,8 @@ def test_chirality_alanine_dipeptide(data):
         pdb = parser.get_structure("", BASE / path)
 
     residue = list(pdb.get_residues())[res_number]
+    
+    assume(residue.get_resname() != 'GLY')
 
     chiral_info = chirality.assign_chirality_amino_acid(residue) 
     if chiral_info == 'L':
