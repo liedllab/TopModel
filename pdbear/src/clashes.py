de8e6ede5abff24dceaf869b3a8b1fae245431be
Janik Kokot <38459178+janikkokot@users.noreply.github.com>
2022-08-24 16:22:36 +0200

    Initial outline for clashes

---
+++ pdbear/src/clashes.py
@@ -0,0 +1,8 @@
+# outline:
+# iterate over residues
+# check wether atoms of residue clash with any of the following residues
+# for residue_i in residues:
+# for residue_j in residues[residue_i:]:
+#
+
+
