d704a300e802de0baa4e38f2006fe7899853f0a8
Janik Kokot <38459178+janikkokot@users.noreply.github.com>
2022-08-30 12:36:44 +0200

    Implement VDW clash detection usind KDTrees

--- pdbear/src/clashes.py
+++ pdbear/src/clashes.py
@@ -1,8 +1,49 @@
-# outline:
-# iterate over residues
-# check wether atoms of residue clash with any of the following residues
-# for residue_i in residues:
-# for residue_j in residues[residue_i:]:
-#
+"""Compute VDW clashes of a Structure"""
+from __future__ import annotations
+
+import itertools
+from scipy import spatial
+from mendeleev import fetch
+
+from Bio.PDB import Structure, Residue, Entity
 
 
+_df = fetch.fetch_table('elements')[['symbol', 'vdw_radius']]
+_df = _df.set_index('symbol')
+VDW_RADII = _df / 100 # conversion from pm to angstrom
+del _df # so it cant be imported anymore?
+
+
+def get_clashes(pdb: Structure.Structure) -> set[frozenset[Residue.Residue]]:
+    """Iterates over structure and yields a set of clashes."""
+    all_clashes = set()
+    _info = ((atom.coord, atom.parent) for atom in pdb.get_atoms())
+    coord, labels = zip(*_info)
+    all_points = spatial.KDTree(coord)
+
+    for residue in pdb.get_residues():
+        clashes = compute_clash(residue, all_points, labels)
+        for clash in clashes:
+            all_clashes.add(frozenset([residue, clash]))
+    return all_clashes
+
+
+def compute_clash(residue: Residue.Residue,
+                  tree: spatial.KDTree,
+                  labels: list[Residue.Residue],
+                  ) -> set[Residue.Residue]:
+    """return clashes of a residue as a set using a KDTree and corresponding labels."""
+    info = ((atom.coord, VDW_RADII.loc[atom.element.capitalize()].values[0])
+            for atom in sidechains(residue))
+    coords, radii = zip(*info)
+    nearby_points = tree.query_ball_point(coords, radii)
+    nearby_labelled = {labels[index] for index in itertools.chain(*nearby_points)}
+
+    return nearby_labelled.difference([residue])
+
+
+def sidechains(entity: Entity.Entity):
+    """Generator that yields sidechain atoms of Entity."""
+    for atom in entity.get_atoms():
+        if atom.name not in {'C', 'CA', 'N', 'HA'}:
+            yield atom
