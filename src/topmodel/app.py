136decf505bbb38729d7318afd8695aab40df992
Janik Kokot <38459178+janikkokot@users.noreply.github.com>
2022-09-29 18:22:40 +0200

    Switch to Clashes only score, update quiet mode
    Now only clashes will be considered for the score as negative impact of
    other irregularities cant be easily assessed.
    
    The output order of the calculated irregularities has been changed and
    the pml output for pymol has been once again fixed.
    
    Mypy 0.981 still has issues with numpy so currently the Mypy checks will
    still fail.

--- src/topmodel/app.py
+++ src/topmodel/app.py
@@ -1,9 +1,8 @@
 #! /usr/bin/env python3
 """Entry point for CLI"""
 from __future__ import annotations
-
+import math
 from enum import Enum
-import itertools
 from pathlib import Path
 import os
 import subprocess
@@ -31,16 +30,6 @@ class App:
     def __init__(self, amides: bool, chiralities: bool, clashes: bool):
         self.funcs: list[Callable[[Structure], dict[Enum, list[StructuralIrregularity]]]] = []
         self.display: dict[Enum, Color] = {}
-        if clashes:
-            self.funcs.append(check.get_clashes)  # type: ignore[attr-defined]
-            self.display.update(
-                        {Clashes.VDW: Color.CYAN},
-                        )
-        if chiralities:
-            self.funcs.append(check.get_chirality)  # type: ignore[attr-defined]
-            self.display.update(
-                        {ChiralCenters.D: Color.MAGENTA},
-                        )
         if amides:
             self.funcs.append(check.get_amide_stereo)  # type: ignore[attr-defined]
             self.display.update({
@@ -48,6 +37,16 @@ class App:
                 AmideBonds.CIS_PROLINE: Color.YELLOW,
                 AmideBonds.NON_PLANAR: Color.YELLOW,
                 })
+        if chiralities:
+            self.funcs.append(check.get_chirality)  # type: ignore[attr-defined]
+            self.display.update(
+                        {ChiralCenters.D: Color.MAGENTA},
+                        )
+        if clashes:
+            self.funcs.append(check.get_clashes)  # type: ignore[attr-defined]
+            self.display.update(
+                        {Clashes.VDW: Color.CYAN},
+                        )
         try:
             self.width = min(80, os.get_terminal_size().columns)
         except OSError:
@@ -55,7 +54,7 @@ class App:
 
         self._n_res: int
         self._data: dict[Enum, list[StructuralIrregularity]]
-        self._score: float
+        self._score: int
 
     @property
     def n_res(self):
@@ -90,8 +89,8 @@ class App:
 
     def output_to_terminal(self) -> None:
         """Organise the output to terminal"""
-
-        if not any(self.data.values()):
+        if not any(self.data[x] for x in (AmideBonds.CIS, AmideBonds.NON_PLANAR,
+                                          AmideBonds.CIS_PROLINE, ChiralCenters.D, Clashes.VDW)):
             click.echo(click.style("No irregularities were found.", bold=True, fg='green'))
 
         for key, values in self.data.items():
@@ -122,16 +121,17 @@ the structure."""
         commands.append('show cartoon')
         commands.append('color white')
 
-        for key, values in self.data.items():
+        for key, values in reversed(self.data.items()):
             sel_string = ' or '.join((entry.to_pymol() for entry in values))
-            try:
-                color = self.display[key].value
-            except KeyError:
-                continue
-            else:
-                commands.append(f'color {color}, {sel_string}')
-                commands.append(f'show sticks, {sel_string}')
-            commands.append(f'select {key.name}, {sel_string}')
+            if sel_string:
+                commands.append(f'select {key.name}, {sel_string}')
+                try:
+                    color = self.display[key].value
+                except KeyError:
+                    continue
+                else:
+                    commands.append(f'color {color}, {key.name}')
+                    commands.append(f'show sticks, {key.name}')
 
         commands.append('color atomic, not elem C')
         commands.append('deselect')
@@ -141,12 +141,9 @@ the structure."""
 
     def compute_score(self) -> None:
         """Assign a score based on the errors in a Structure."""
-        raw_score = sum(
-            (entry.score for entry in itertools.chain(*self.data.values()))
-            )
-        # if every 3rd AA has an error the score would be roughly 0.
-        score = 1 - (3*raw_score / self.n_res)
-        self._score = score
+        raw_score = len(self.data[Clashes.VDW])
+        score = 30*raw_score // math.sqrt(self.n_res)
+        self._score = int(score)
         return None
 
     def __repr__(self):
@@ -163,7 +160,7 @@ the structure."""
 @click.option("--chiralities/--no-chiralities", is_flag=True, default=True)
 @click.option("--clashes/--no-clashes", is_flag=True, default=True)
 @click.option("--score/--no-score", is_flag=True, default=False)
-@click.option("--quiet/--verbose", is_flag=True, default=False)
+@click.option("--quiet/--verbose", is_flag=True, default=False, help='Start PyMOL quietly')
 @click.option("--pymol", is_flag=True, default=False, show_default=True,
               help=('Open the structure in PyMOL with the irregularities annotated. '
                     'Requires pymol to be in path.')
@@ -194,17 +191,31 @@ def main(files: list[str],
             click.echo(click.style(error, fg='white', bg='red', bold=True))
             raise click.Abort() from error
 
-        if not quiet:
-            app.output_to_terminal()
+        app.output_to_terminal()
         if score:
             app.compute_score()
-            click.echo(click.style('\nSCORE:\t', bold=True) + f'{app.score:#.2%}')
+            if app.score == 0:
+                score_color = 'green'
+            elif app.score < 10:
+                score_color = 'yellow'
+            else:
+                score_color = 'red'
+            click.echo(click.style('\nCLASH SCORE: ', bold=True) +
+                       click.style(f'{app.score:>3d}', bold=True, fg=score_color))
         if pymol:
             pml = app.to_pml(path)
             with tempfile.NamedTemporaryFile(mode='w', suffix='.pml') as tmp:
                 tmp.write(pml)
                 tmp.flush()
-                subprocess.call(('pymol', '-Q', tmp.name))
+                command = 'pymol -Q' if quiet else 'pymol'
+                subprocess.call((*command.split(), tmp.name))
+                if not quiet:
+                    click.echo(f'{"SUMMARY":=^{app.width}}')
+                    app.output_to_terminal()
+                    if score:
+                        click.echo(click.style('\nCLASH SCORE: ', bold=True) +
+                                   click.style(f'{app.score:>3d}', bold=True, fg=score_color))
+
     click.echo('-'*app.width)
 
 
