bd771566dbbcb9411d0f83a3298eafaa90be6b09
Janik <38459178+janikkokot@users.noreply.github.com>
2024-04-22 16:44:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 if __name__ == '__main__':
     setup(
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+        name='protein-topmodel',
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
     )
